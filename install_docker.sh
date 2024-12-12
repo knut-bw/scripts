23e61e09ecd447da692ca3d05d6c29940525cf91
knut-bw <88424915+knut-bw@users.noreply.github.com>
2024-12-12 13:51:13 +0800

    Create install_docker.sh

---
+++ install_docker.sh
@@ -0,0 +1,42 @@
+# install docker enginee(https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository)
+# Add Docker's official GPG key:
+sudo apt-get -y update
+sudo apt-get -y install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get -y update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo mkdir -p /etc/docker
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+# Manage Docker as a non-root user(https://docs.docker.com/engine/install/linux-postinstall/#manage-docker-as-a-non-root-user)
+sudo systemctl daemon-reload
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo chmod 666 /var/run/docker.sock
+#sudo newgrp docker
+sudo systemctl start docker
+sudo systemctl enable docker
+
+docker --version
+
+# 已有Dokcer enginee & cli 情況下 安裝 Docker-compose
+sudo -S curl -SL https://github.com/docker/compose/releases/download/v2.20.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod 755 /usr/local/bin/docker-compose
